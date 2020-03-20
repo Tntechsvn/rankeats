27bd5adcadd5b14f9a3150b4c284e3511787cbac
PhongNguyen <s2nhomau@gmail.com>
2020-03-20 10:21:25 +0700

    20-03-2020 v1

--- resources/views/layouts/form-search.blade.php
+++ resources/views/layouts/form-search.blade.php
@@ -10,6 +10,9 @@
 	      <input type="text" class="form-control input-lg location_search" id="location_search" placeholder="@if(isset($city) && $city !=''){{$city.','.$state}}@else{{'City or State'}}@endif">
 	      <input  type="hidden" id="city_search" name="city" value="@if(isset($city)){{$city}}@endif">
 	      <input  type="hidden" id="state_search" name="state" value="@if(isset($state)){{$state}}@endif">
+	      <div id="locationField">
+	      	<input  type="hidden" id="autocomplete" value="all" />
+	      </div>
 	    </div>
 	    <span class="input-group-btn">
 	      <button type="submit" class="btn btn-custom input-lg" style="border-radius: 0;"><span class="glyphicon glyphicon-search"></span><span class="text-search">Search</span></button>
