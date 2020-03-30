1e43e7b53c1911cc5276b2cbcd83044e5b182cc7
hungpro <tuanhungk7c@gmail.com>
2020-03-30 18:24:58 +0700

    single business

--- resources/views/layouts/single-business.blade.php
+++ resources/views/layouts/single-business.blade.php
@@ -130,7 +130,8 @@
 					    <div class="tab-pane" id="eatrank">
 					    	@if($list_reviews->total() >0)
 				    			@foreach($list_reviews as $data)
-							    	<p>
+				    			<div class="list-review">
+				    				<p>
 							    		<i class="fas fa-user"></i>
 							    		<span class="bold">Name :</span>
 							    		<span class="bold" style="color: #0073bb">{{$data->user->name}}</span>
@@ -145,22 +146,33 @@
 										</span>
 							    	</p>
 							    	<div >
-							    		<label class="m-r-30"><i class="fas fa-globe-americas" style="color: "></i> Country: {{$info_business->location->country}} (12)</label>
-							    		<label class="m-r-30"><i class="fas fa-city"></i> City: {{$info_business->location->city}} (12)</label>
-							    		<label><i class="fas fa-city"></i> State: {{$info_business->location->state}} (12)</label>
+							    		<label class="m-r-30"><i class="fas fa-globe-americas" style="color: "></i> Country: {{$data->business->location->country}} (12)</label>
+							    		<label class="m-r-30"><i class="fas fa-city"></i> City: {{$data->business->location->city}} (12)</label>
+							    		<label><i class="fas fa-city"></i> State: {{$data->business->location->state}} (12)</label>
 							    	</div>
 
 							    	<p>{{$info_business->description}}</p>
 							    	<div class="star-view clear p-b-10">
-							    		
-												
-										@for($i = 1;$i <= $data->review_rating->where('review_id','=',$data->id)->first()->rate;$i++)
-											<i class="fas fa-star star-rate"></i>
-										@endfor
-							    		
-
-							    		
+							    		<ul class="star-rate">
+								    		@php
+									    		$star_review = $data->review_rating->where('review_id','=',$data->id)->first()->rate;
+												$val =  (int) substr(strrchr($star_review,'.'),1);
+												for($x=1;$x<=$star_review;$x++) {
+													echo '<li><i class="fas fa-star star-icon " aria-hidden="true"></i></li>';
+												}
+												if (strpos($star_review,'.') && $val != 0) {
+													echo '<li><i class="fas fa-star-half-alt star-icon " aria-hidden="true"></i></li>';
+													$x++;
+												}
+												while ($x<=5) {
+													echo '<li><i class="far fa-star star-icon " aria-hidden="true"></i></li>';
+													$x++;
+												}
+											@endphp
+										</ul>
 									</div>
+				    			</div>
+							    	
 
 								@endforeach
 				    		@endif
