03391991569f5f04553ad81a47c12a604f956260
hungpro <tuanhungk7c@gmail.com>
2020-04-11 20:53:13 +0700

    add code phong

--- resources/views/layouts_profile/bookmark.blade.php
+++ resources/views/layouts_profile/bookmark.blade.php
@@ -11,14 +11,14 @@
 
 	<div class="container  content-profile">
 		<div class="row">
-			<div class="col-md-3">
+			<div class="col-md-3 col-lg-3 col-sm-5 col-xs-12 menu-sidebar">
 
 				<div class="profile-usermenu">
 					@include('layouts_profile.menu-sidebar')
 				</div>
 			</div>
 
-			<div class="col-md-9 content-search">
+			<div class="col-md-9 col-lg-9 col-sm-7 col-xs-12 content-search content-right-profile">
 				<h3 class="title">My Bookmarks</h3>
 				<div class="clear"></div>	
 				@foreach($data_business as $data)			
--- resources/views/layouts_profile/create-advertise.blade.php
+++ resources/views/layouts_profile/create-advertise.blade.php
@@ -11,14 +11,14 @@
 
 	<div class="container  content-profile">
 		<div class="row">
-			<div class="col-md-3 col-lg-3 col-sm-5 col-xs-12">
+			<div class="col-md-3 col-lg-3 col-sm-5 col-xs-12 menu-sidebar">
 
 				<div class="profile-usermenu">
 					@include('layouts_profile.menu-sidebar')
 				</div>
 			</div>
 
-			<div class="col-md-9 col-lg-9 col-sm-7 col-xs-12 content-search">
+			<div class="col-md-9 col-lg-9 col-sm-7 col-xs-12 content-search content-right-profile">
 				
 				<h3 class="title">Business Advertisement</h3>
 				<div class="clear"></div>
